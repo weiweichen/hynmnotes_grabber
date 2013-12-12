883ce9e2f33b32c74d2a4a56727d3d3e90650ef0
Weiwei Chen <weiwei.chen@uci.edu>
2013-12-11 23:45:40 -0800

    update 1

--- hymn_grabber.sh
+++ hymn_grabber.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+if [ -d "notes" ]
+then
+	echo "found notes"
+else
+	mkdir notes
+fi
+
 cat "keywords.txt" | while read KEYWORD
 do
-	echo $KEYWORD
-	URL="http://www.hymnal.net/en/search.php/all/all/${KEYWORD}"
-	wget -U safari -O "song_${KEYWORD}.tmp" "$URL" -e robots=off
+	SEARCHKEY=`echo ${KEYWORD} | sed 's/\( \)\{1,\}/%20/g'`
+	echo ${SEARCHKEY}
+	URL="http://www.hymnal.net/en/search.php/all/all/${SEARCHKEY}"
+	wget -U safari -O "song.tmp" "$URL" -e robots=off
 	COUNTER=0
-	cat "song_${KEYWORD}.tmp" | grep "hymn.php" | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=['"'"'"]//' -e 's/["'"'"']$//' | while read PAGE
+	cat "song.tmp" | grep "hymn.php" | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=['"'"'"]//' -e 's/["'"'"']$//' | while read PAGE
 	do
 		let "COUNTER+=1"
 		if [ "$COUNTER" -gt "2" ]
@@ -16,7 +24,6 @@ do
 		wget -U safari -O "page.tmp" "$PAGE" -e robots=off
 		cat "page.tmp" | grep Hymns | grep -o '<span>http://[^"'"'"']*' | grep g.png | sed -e 's/^<span>//' -e 's/<\/span>//' | tr -d '\r' |  while read -r out
 		do
-#			echo $out
 			echo $out | rev | awk -F \/ '{print $1}' | rev | while read -r filename
 			do
 				echo $filename
